ebb08ecd5c6ca838a8cd4c262a345882fc800baa
ItSrGop <gabriel.gopp@gmail.com>
2024-05-13 15:55:06 -0300

    up

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 const { WebSocketServer } = require("ws")
-const dotenv = resquire("dotenv")
+const dotenv = require("dotenv")
 
 dotenv.config()
 
@@ -8,8 +8,10 @@ const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 wss.on("connection", (ws) => {
     ws.on("error", console.error)
 
+
     ws.on("message", (data) => {
         wss.clients.forEach((client) => client.send(data.toString()))
     })
+
     console.log("client connected")
 })
